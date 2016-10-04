186f900d042ad46906f0a4019ebec931d85dbb3f
Brayan Gallardo <brgallar@uci.edu>
2016-10-04 08:19:25 -0700

    commit/upload

---
+++ home.js
@@ -0,0 +1,4 @@
+function expandContent(id) {
+	var content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non risus sed urna bibendum pellentesque sed sagittis sapien. Sed accumsan efficitur ligula, vitae hendrerit nisi aliquam viverra. Donec gravida, metus vitae accumsan sodales, ante eros lobortis nunc, ut luctus lacus nunc quis mauris. Fusce sit amet elit imperdiet, ultricies odio vitae, cursus ipsum. Mauris sed augue eu enim lobortis fringilla. Sed orci mauris, pulvinar in mauris a, m";
+	document.getElementById(id).innerHTML = content;
+}
